2cbad29ec4f76d469b519fe531876d7ff9aa8973
kyworkspace <ky91zzang@naver.com>
2022-07-01 00:04:36 +0900

    env update

--- src/main/java/com/board/project/WebMvcConfig.java
+++ src/main/java/com/board/project/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.board.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,11 +13,15 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @PropertySource("classpath:properties/env.properties")//env 파일 등록
 public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	private Environment env;
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		// cors open => front client(react)에서 기본 3000번 포트 사용
 		registry.addMapping("/**")
-		.allowedOrigins("${front.host}:${front.port}")
+		.allowedOrigins(env.getProperty("front.host") +":"+env.getProperty("front.port"))
 		.allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
 	}
 }
